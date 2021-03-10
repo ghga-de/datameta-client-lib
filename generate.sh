8a91db14e154160ff9274555d8ad7542c9764d2f
KerstenBreuer <kersten-breuer@outlook.com>
2021-03-10 11:30:43 +0100

    script to generate client fro openapi spec

---
+++ generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR_OF_THIS_SCRIPT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+OPENAPI_SOURCE="https://raw.githubusercontent.com/ghga-de/datameta/main/datameta/api/openapi.yaml"
+
+docker run --rm \
+    -v "${DIR_OF_THIS_SCRIPT}:/datameta_client_lib" \
+    openapitools/openapi-generator-cli \
+        generate \
+        -i "${OPENAPI_SOURCE}" \
+        -g python \
+        --package-name datameta_client_lib \
+        -o /datameta_client_lib
